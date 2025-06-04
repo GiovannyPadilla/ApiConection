4de79dcd7d3345316d93100fc5a9faf418126aa6
GiovannyPadilla <146996637+GiovannyPadilla@users.noreply.github.com>
2025-06-04 07:35:44 -0500

    CAmbios usuariocontroller

--- ApiConection/Models/Mascota.cs
+++ ApiConection/Models/Mascota.cs
@@ -8,7 +8,7 @@
         public string Raza { get; set; }
         public int Edad { get; set; }
         public int UsuarioId { get; set; }
-        public Usuario Usuario { get; set; }
+        public Usuario? Usuario { get; set; }
     }
 
 }
--- ApiConection/Models/Usuario.cs
+++ ApiConection/Models/Usuario.cs
@@ -3,9 +3,11 @@
     public class Usuario
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string RUsuario { get; set; }
+        public string Contrasena { get; set; }
         public string Correo { get; set; }
-
+        public string telefono { get; set; }
+        public string Direccion { get; set; }
         public ICollection<Mascota> Mascotas { get; set; }
     }
 
